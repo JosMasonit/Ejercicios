cf69133cc04bd2f4d2c80b31e161f172fdf33d37
JosMasonit <108924443+JosMasonit@users.noreply.github.com>
2022-07-21 19:30:53 +0200

    Add files via upload

---
+++ SPRING/BS5/src/main/java/com/BS5/BS5/Persona.java
@@ -0,0 +1,12 @@
+package com.BS5.BS5;
+
+import lombok.Data;
+
+@Data
+public class Persona {
+
+    private String nombre;
+    private String poblacion;
+    private int edad;
+
+}
---
+++ SPRING/BS5/src/main/java/com/BS5/BS5/PersonaService.java
@@ -0,0 +1,11 @@
+package com.BS5.BS5;
+
+public interface PersonaService {
+    String getNombre();
+    String getPoblacion();
+    int getEdad();
+    void setNombre(String nombre);
+    void setPoblacion(String poblacion);
+    void setEdad(int edad);
+    void MultiplicaEdad();
+}
